b1863341474bb26e870b9637307720849f090baf
minhchi972000 <maiminhchi972000@gmail.com>
2021-12-29 19:51:28 +0700

    Smarthome

--- app/src/main/java/com/first_java_app/smarthrt/Music.java
+++ app/src/main/java/com/first_java_app/smarthrt/Music.java
@@ -4,8 +4,10 @@ package com.first_java_app.smarthrt;
 import android.app.Service;
 import android.content.Intent;
 import android.media.MediaPlayer;
+import android.os.CountDownTimer;
 import android.os.IBinder;
 import android.util.Log;
+import android.widget.Toast;
 
 import androidx.annotation.Nullable;
 
@@ -35,15 +37,21 @@ public class Music extends Service {
         if(id == 1){
             mediaPlayer =MediaPlayer.create(this,R.raw.nokia_tune_original_ringtone_alarm);
             mediaPlayer.start();
+            new CountDownTimer(2000, 100) {
+                @Override
+                public void onTick(long millisUntilFinished) {
+                    Toast.makeText(Music.this, "Vui lòng tắt báo thức", Toast.LENGTH_SHORT).show();
+                }
+                @Override
+                public void onFinish() {
+
+                }
+            }.start();
             id=0;
         }else if(id==0){
             mediaPlayer.stop();
             mediaPlayer.reset();
         }
-
-
-
-
         return START_NOT_STICKY;
     }
 }
